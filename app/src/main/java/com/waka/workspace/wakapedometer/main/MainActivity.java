b9e82030751e4739af95032871afe28e302ccaff
BadWaka <1456683844@qq.com>
2016-02-16 18:08:34 +0800

    沉浸式状态栏

--- app/src/main/java/com/waka/workspace/wakapedometer/main/MainActivity.java
+++ app/src/main/java/com/waka/workspace/wakapedometer/main/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -44,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     /*initView*/
     private void initView() {
+
         viewPager = (ViewPager) findViewById(R.id.viewPager);
     }
 
